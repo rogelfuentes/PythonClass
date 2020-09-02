e4aecb2c0a94164212f5f94f832d0127539aafd7
Rogel Fuentes <rogelfuentes@gmail.com>
2020-09-02 17:58:28 -0600

    Adding more exercices in the week2

---
+++ Week2/voting.py
@@ -0,0 +1,7 @@
+age = 15
+if age >= 18:
+    print("He can vote")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
