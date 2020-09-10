5accc34ccb128ae8304ee996184fa39b640825e3
Rogel Fuentes <rogelfuentes@gmail.com>
2020-09-09 23:09:19 -0600

    The in class exercise has been done and submited

--- Week3/InClassExercise3-ForLoops.py
+++ Week3/InClassExercise3-ForLoops.py
@@ -4,12 +4,13 @@
 # Asks the user for how many numbers they want to add.  Reads in each number and prints a total at the end.
 # Hint: use the range() function: for i in range(quantity):
 
-addNum = int(input("How many numbers do you want to add? :"))
+totalNum = int(input("How many numbers do you want to add? :"))
+totalAddNum = 0
+for i in range(totalNum): 
+    addNum = float(input("what is the number to add?: "))
+    totalAddNum += addNum
+print(f"The total sum that you has been adding is: {totalAddNum}")
 
-
-
-
- 
 
 # #2 Find the vowels – for loop (4.5 points)
 # Using the 'if' statement and the 'or' operator, write a program that uses the input() function and asks the user for a word or sentence.
@@ -18,19 +19,19 @@ addNum = int(input("How many numbers do you want to add? :"))
 
 # Pseudo code:
 # Prompt user for a word or phrase.
-# total = 0
+wordPhrase = str(input("Type a word or a phrase: "))
+total = 0
+vowelList = []
+
 # enter 'for' loop:
-# for each letter, check if the letter is an 'a,e,i,o or u'. 
+for i in wordPhrase:
+# for each letter, check if the letter is an 'a,e,i,o or u'.
+    if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
 # If a match, then +1 to total.
+        total += 1
+        vowelList.append(i)
 # end loop.
+
 # Print total results.
- 
-
-# Pront user input
-# How many times?
-# for each time:
-#     value=input "enter ....."
-#     total += value 
-
-#     print total
-
+print(total)
+print(f"The vowels in the list are:{vowelList}")
